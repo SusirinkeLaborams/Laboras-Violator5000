a1881e8f6046562d9fdc7782bf3e15d2f26e22b0
DariusL <d.lapunas@gmail.com>
2014-10-05 20:01:38 +0300

    Changed ReceverImpl to a class template

--- Laboras-Violator5000/RobotInterface/ReceiverImpl.h
+++ Laboras-Violator5000/RobotInterface/ReceiverImpl.h
@@ -1,32 +1,36 @@
+#pragma once
 #include "PrecompiledHeader.h"
 
+template<typename Robot>
 class ReceiverImpl
 {
 	std::thread loopThread;
 	volatile bool running;
 public:
 	template<typename Callback>
-	void StartReceiving(const Callback &callback);
+	void StartReceiving(Callback callback);
 	void StopReceiving();
 
 private:
 	template<typename Callback>
-	void StartInternal(const Callback &callback);
+	void StartInternal(Callback callback);
 };
 
 //not thread safe
+template<typename Robot>
 template<typename Callback>
-void ReceiverImpl::StartReceiving(const Callback &callback)
+void ReceiverImpl<Robot>::StartReceiving(Callback callback)
 {
 	//joinable == running
 	Assert(!(loopThread.joinable() || running));
 	running = true;
 
-	loopThread = std::thread(std::bind(&ReceiverImpl::StartInternal, this, std::ref(callback)));
+	loopThread = std::thread(std::bind(&ReceiverImpl::StartInternal, this, callback));
 }
 
+template<typename Robot>
 template<typename Callback>
-void ReceiverImpl::StartInternal(const Callback &callback)
+void ReceiverImpl<Robot>::StartInternal(Callback callback)
 {
 	while (running)
 	{
@@ -34,9 +38,10 @@ void ReceiverImpl::StartInternal(const Callback &callback)
 	}
 }
 
-void ReceiverImpl::StopReceiving()
+template<typename Robot>
+void ReceiverImpl<Robot>::StopReceiving()
 {
 	running = false;
 }
 
-using Receiver = ReceiverImpl;
+using Receiver = ReceiverImpl<int>;//dummy param
