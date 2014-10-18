5433e82ee5f8b3646b74546a927320964316455e
Sunius <suniusx@gmail.com>
2014-10-18 17:41:03 +0300

    Add uncommited file.

---
+++ Laboras-Violator5000/Utilities/CriticalSection.h
@@ -0,0 +1,44 @@
+class CriticalSection
+{
+private:
+	CRITICAL_SECTION m_Section;
+
+public:
+	CriticalSection()
+	{
+		InitializeCriticalSection(&m_Section);
+	}
+
+	~CriticalSection()
+	{
+		DeleteCriticalSection(&m_Section);
+	}
+
+	void Enter()
+	{
+		EnterCriticalSection(&m_Section);
+	}
+
+	void Leave()
+	{
+		LeaveCriticalSection(&m_Section);
+	}
+
+	class Lock
+	{
+	private:
+		CriticalSection& m_CriticalSection;
+
+	public:
+		Lock(CriticalSection& criticalSection) :
+			m_CriticalSection(criticalSection)
+		{
+			m_CriticalSection.Enter();
+		}
+
+		~Lock()
+		{
+			m_CriticalSection.Leave();
+		}
+	};
+};
