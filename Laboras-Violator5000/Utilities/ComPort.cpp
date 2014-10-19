a1c9730f67178b4b91b5f9e3641b7d20d85fbb5e
DariusL <d.lapunas@gmail.com>
2014-10-19 22:05:11 +0300

    A serial port wraper

---
+++ Laboras-Violator5000/Utilities/ComPort.cpp
@@ -0,0 +1,37 @@
+#include "PrecompiledHeader.h"
+#include "ComPort.h"
+
+
+ComPort::ComPort(std::string name)
+	:name(name)
+{
+	Open();
+}
+
+
+ComPort::~ComPort()
+{
+	Close();
+}
+
+bool ComPort::Open()
+{
+	handle = CreateFileA(
+		name.c_str(),
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		0,
+		OPEN_EXISTING,
+		FILE_FLAG_OVERLAPPED,
+		0);
+	open = (handle != INVALID_HANDLE_VALUE);
+}
+
+void ComPort::Close()
+{
+	if (open)
+	{
+		CloseHandle(handle);
+		open = false;
+	}
+}
---
+++ Laboras-Violator5000/Utilities/ComPort.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "PrecompiledHeader.h"
+class ComPort
+{
+	HANDLE handle;
+	bool open;
+	std::string name;
+public:
+	bool Open();
+	void Close();
+	bool IsOpen(){ return open; }
+
+	template<typename T>
+	T Read();
+
+	template<typename T>
+	void Write(const T &data);
+
+	ComPort(std::string name);
+	~ComPort();
+};
+
+template<typename T>
+T ComPort::Read()
+{
+	//fail if closed
+	Assert(open);
+
+	T ret;
+	DWORD dwRead;
+	OVERLAPPED reader = { 0 };
+	reader.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	Assert(reader.hEvent != NULL);
+
+	if (ReadFile(handle, &ret, sizeof(T), &dwRead, &reader))
+	{
+		//fail if read wrong amount of stuff
+		Assert(dwRead == sizeof(T));
+	}
+	else
+	{
+		Assert(GetLastError() == ERROR_IO_PENDING);
+
+		switch (WaitForSingleObject(reader, 2000))
+		{
+		case WAIT_OBJECT_0:
+			Assert(GetOverlappedResult(handle, &reader, &dwRead, FALSE));
+			Assert(dwRead == sizeof(T));
+		case WAIT_TIMEOUT:
+		default:
+			Assert(false);
+			break;
+		}
+	}
+
+	CloseHandle(reader.hEvent);
+	return ret;
+}
+
+template<typename T>
+void ComPort::Write(const T &data)
+{
+	OVERLAPPED writer = { 0 };
+	DWORD written;
+	writer.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	Assert(writer.hEvent != NULL);
+
+	if (!WriteFile(handle, &data, sizeof(T), &written, TRUE)
+	{
+		Assert(GetLastError() == ERROR_IO_PENDING);
+		Assert(WaitForSingleObject(writer, INFINITE) == WAIT_OBJECT_0);
+		Assert(GetOverlappedResult(handle, writer, &written, FALSE));
+		Assert(written == sizeof(T));
+	}
+
+	CloseHandle(writer.hEvent);
+}
